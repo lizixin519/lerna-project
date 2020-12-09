952dac7b2cf01956332e766991261f3685155718
李紫鑫 <lizixin@boco.com.cn>
2020-12-09 19:43:58 +0800

    test

--- packages/demo-core/lib/demo-core.js
+++ packages/demo-core/lib/demo-core.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-12-09 16:59:53
- * @LastEditTime: 2020-12-09 17:11:36
- * @LastEditors: your name
+ * @LastEditTime: 2020-12-09 19:43:47
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /lerna-project/packages/demo-core/lib/demo-core.js
  */
 'use strict';
 
 function add(a, b) {
-    return a + b;
+    return (a + b) / 2;
 }
 export default add;
